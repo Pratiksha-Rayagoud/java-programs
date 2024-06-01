8c4de118d7829a0d5a29d266999024c76c5195e1
Pratiksha-Rayagoud <patildhangounda@gmail.com>
2024-06-01 14:03:29 +0530

    second commit

--- CarClass.java
+++ CarClass.java
@@ -1,33 +1,34 @@
-class Car {
+ class Car 
+{
 
   private String make;
   private String model;
   private int year;
 
-
-  public Car(String make, String model, int year) {
+  public Car(String make, String model, int year) 
+{
     this.make = make;
     this.model = model;
     this.year = year;
   }
 
-  public void printDetails() {
+  public void printDetails() 
+{
     System.out.println("Make: " + make);
     System.out.println("Model: " + model);
     System.out.println("Year: " + year);
   }
 }
+public class CarClass
+{
 
-public class CarClass {
-
-  public static void main(String[] args) {
-  
+  public static void main(String[] args) 
+{
     Car car1 = new Car("Toyota", "Camry", 2023);
     Car car2 = new Car("Honda", "Civic", 2020);
 
-
     car1.printDetails();
-    System.out.println();
+    System.out.println(); 
     car2.printDetails();
   }
 }
